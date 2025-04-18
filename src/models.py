34143fb3308a0031ca38e1cdfff2b426ec9558b6
Alexander Morales <alexandermorales.n1@gmail.com>
2025-04-18 04:18:06 +0000

    add corrected models

--- src/models.py
+++ src/models.py
@@ -1,19 +1,77 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import String, Boolean
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import String, Boolean, ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 db = SQLAlchemy()
 
 class User(db.Model):
+    __tablename__ = 'users'
+
     id: Mapped[int] = mapped_column(primary_key=True)
     email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
     password: Mapped[str] = mapped_column(nullable=False)
     is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
 
+    favorites: Mapped[list['Favorite']] = relationship('Favorite', back_populates = 'user')
 
     def serialize(self):
         return {
             "id": self.id,
             "email": self.email,
-            # do not serialize the password, its a security breach
+        }
+
+class Character(db.Model):
+    __tablename__ = 'characters'
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(unique=True, nullable=False)
+    hair_color: Mapped[str] = mapped_column(nullable=False)
+    eye_color: Mapped[str] = mapped_column(nullable=False)
+    gender: Mapped[str] = mapped_column(nullable=False)
+
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "hair_color" : self.hair_color,
+            "eye_color": self.eye_color,
+            "gender": self.gender,
+        }
+    
+class Planet(db.Model):
+    __tablename__ = 'planets'
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(unique=True, nullable=False)
+    diameter: Mapped[float] = mapped_column(nullable=False)
+    population: Mapped[int] = mapped_column(nullable=False)
+    gravity: Mapped[float] = mapped_column(nullable=False)
+
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name" : self.name,
+            "diameter" : self.diameter,
+            "population": self.population,
+            "gravity": self.gravity,
+
+        }
+
+class Favorite(db.Model):
+    __tablename__ = 'favorites'
+    id: Mapped[int] = mapped_column(primary_key=True)
+    user_id: Mapped[int] = mapped_column(ForeignKey("users.id"), nullable=False)
+    planet_id: Mapped[int] = mapped_column(ForeignKey("planets.id"), nullable=False)
+    character_id: Mapped[int] = mapped_column(ForeignKey("characters.id"), nullable=False)
+
+    user: Mapped['User'] = relationship('User', back_populates = 'favorites')
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "user_id" : self.user_id,
+            "planet_id": self.planet_id,
+            "character_id": self.character_id,
+
         }
